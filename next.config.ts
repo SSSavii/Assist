563e0323a14cb67cceeb5fb435e3794a636b0868
SSSavii <georgiii3512@mail.ru>
2025-10-03 21:42:31 +0700

    фикс

--- next.config.ts
+++ next.config.ts
@@ -9,13 +9,8 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
-  swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
-  },
-  // Оптимизация для быстрых переходов
-  experimental: {
-    optimizeCss: true,
   },
 };
 
