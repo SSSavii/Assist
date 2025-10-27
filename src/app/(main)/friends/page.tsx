00b9a95066379bd515526d3eff3d518c2ab5b557
SSSavii <georgiii3512@mail.ru>
2025-10-27 23:59:10 +0700

    проверка

--- src/app/(main)/friends/page.tsx
+++ src/app/(main)/friends/page.tsx
@@ -361,7 +361,7 @@ export default function FriendsPage() {
             width: 100%;
             position: relative;
             height: 300px;
-            margin-bottom: -180px;
+            margin-bottom: -100px;
             z-index: 0;
           }
 
@@ -773,7 +773,7 @@ export default function FriendsPage() {
             }
 
             .header-section {
-              margin-bottom: -160px;
+              margin-bottom: -80px;
             }
 
             .info-label,
